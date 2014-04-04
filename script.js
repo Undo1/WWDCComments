dcecc9c1ec6b618da5b320a1a6b02ea66ffa05ee
Undo1 <parker@erwaysoftware.com>
2014-04-03 18:51:01 -0600

    add name/occ to script

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
 	$(".submit-comment-button").click(function() {
 		
-		var argString = "text=" + $(".comment-text-area").val();
+		var argString = "text=" + $(".comment-text-area").val() + "&name=" + $("#nameInput").val() + "&occ=" + $("#occupationInput").val();
 
 		$.ajax({
 		    type: "GET",
