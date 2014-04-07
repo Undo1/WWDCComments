e6bfff591c57e7f9ee850299d66d5ced1ccfe330
Undo1 <parker@erwaysoftware.com>
2014-04-07 14:15:56 -0600

    stuff!

--- app/submitfeedback.php
+++ app/submitfeedback.php
@@ -6,3 +6,15 @@
 		echo 'ha, nice try';
 		return;
 	}
+
+	$text = $_POST["text"];
+
+	echo $text;
+
+	$dbhost = MySQLHost(); // this will ususally be 'localhost', but can sometimes differ  
+	$dbname = MySQLDB(); // the name of the database that you are going to use for this project  
+	$dbuser = MySQLUsername(); // the username that you created, or were given, to access your database  
+	$dbpass = MySQLPassword(); // the password that you created, or were given, to access your database
+
+	$db = new PDO('mysql:host=' . $dbhost . ';dbname=' . $dbname . ';charset=utf8', $dbuser, $dbpass);
+	$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
